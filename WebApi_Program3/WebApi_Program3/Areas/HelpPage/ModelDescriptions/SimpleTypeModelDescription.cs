31cf877cf6483eabde769f4e26a91a24bbfd3755
Bilal <bilalahmadshah47@gmail.com>
2018-06-13 13:19:12 +0530

    Web Api program _Program3

---
+++ WebApi_Program3/WebApi_Program3/Areas/HelpPage/ModelDescriptions/SimpleTypeModelDescription.cs
@@ -0,0 +1,6 @@
+namespace WebApi_Program3.Areas.HelpPage.ModelDescriptions
+{
+    public class SimpleTypeModelDescription : ModelDescription
+    {
+    }
+}
---
+++ WebApi_Program3/WebApi_Program3/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="WebApi_Program3.WebApiApplication" Language="C#" %>
